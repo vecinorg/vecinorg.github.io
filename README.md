980fa57286c15b01d6e5db1323e58a85cf66ea0e
Leonardo <leonardo.nieto@vecino.com.co>
2014-11-14 20:52:29 -0500

    gh-page + startbootstrap-clean-blog-jekyll para vecino

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-vecino
+# This is the webpage for Vecino
+
+## We are using a modified version of this code
+
+### Clean Blog by Start Bootstrap - Jekyll Version
+
+The official Jekyll version of the Clean Blog theme by [Start Bootstrap](http://startbootstrap.com/).
+
+Visit Clean Blog's template overview page on Start Bootstrap at http://startbootstrap.com/template-overviews/clean-blog/ and leave a comment, email feedback@startbootstrap.com, or open an issue here on GitHub for support.
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,9 +17,9 @@ $message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'info@vecino.com.co'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_subject = "Contacto página web:  $name";
+$email_body = "Hemos recibido un mensaje nuevo desde el formulario de contacto de la página web.\n\n"."Detalles:\n\nNombre: $name\n\nCorreo: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
+$headers = "From: noreply@vecino.com.co\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
